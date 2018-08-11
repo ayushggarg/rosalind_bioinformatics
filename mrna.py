95efcfcd2c582ece46ad60c78e89ecaad233b874
ayushggarg <ayushggarg@gmail.com>
2018-08-12 00:39:59 +0530

    added problem mrna in python

---
+++ mrna.py
@@ -0,0 +1,40 @@
+map = {"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+    "UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
+    "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
+    "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
+    "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+    "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+    "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+    "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+    "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+    "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+    "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+    "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+    "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+    "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+    "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+    "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G"}
+
+def codon_frequency():
+	frequencies = {}
+	for k,v in map.items():
+		if v not in frequencies:
+			frequencies[v] = 0
+		frequencies[v] += 1
+	return frequencies
+
+file = open("rosalind_mrna.txt", "r")
+data = file.read().splitlines()
+data = data[0]
+file.close()
+
+#data = "MA"
+
+frequencies = codon_frequency()
+count = frequencies['STOP']
+
+for letter in data:
+	count *= frequencies[letter]
+	count %= 1000000
+
+print (count)
